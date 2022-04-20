0981297012faf50c4f5737e27edf42e5fc9bd23b
LJosueCM <ljosuecm@gmail.com>
2022-04-20 18:22:15 -0500

    Segundo requerimiento

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,12 +2,12 @@ const User = require('../../app/models/User')
 
 describe('Unit test for User class',()=>{
     test('Create an User object', () => {
-        const user = new User(1,'blu3ming','Josue Cruz','Computer Engineer. Developer.',0,0);
+        const user = new User(1,'blu3ming','Josue Cruz','Computer Engineer. Developer.');
         expect(user.id).toBe(1);
         expect(user.username).toBe('blu3ming');
         expect(user.name).toBe('Josue Cruz');
         expect(user.bio).toBe('Computer Engineer. Developer.');
-        expect(user.dateCreated).toBe(0);
-        expect(user.lastUpdated).toBe(0);
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     });
 });
