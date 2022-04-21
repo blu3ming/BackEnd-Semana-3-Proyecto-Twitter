281ed3230770aca56a1c107e51a1f158d98659f0
LJosueCM <ljosuecm@gmail.com>
2022-04-20 23:18:41 -0500

    Views Requerimiento 1 Exitoso

---
+++ app/views/UserView.js
@@ -0,0 +1,9 @@
+const UserService = require('../../app/services/UserService')
+
+class UserView{
+    static createUser(payload){
+        if(payload === null){return {error:"El payload no existe"}}
+    }
+}
+
+module.exports = UserView
