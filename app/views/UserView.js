755e5c48bcca9b81a784efa5c2167ff2cb38a3bc
LJosueCM <ljosuecm@gmail.com>
2022-04-20 23:45:10 -0500

    Views Corrección en mensaje error Requerimiento 3

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -6,7 +6,7 @@ class UserView{
         else if(typeof payload.username != "string" && typeof payload.name != "string" && typeof payload.id != "number"){
             return {error: "Los valores del payload necesitan tener un valor válido"}
         } else if(payload.username === undefined || payload.name === undefined || payload.id === undefined){
-            return {error: "Los valores del payload necesitan tener un valor válido2"}
+            return {error: "Los valores del payload necesitan tener un valor válido"}
         } else{
             const user = UserService.create(payload.id, payload.username, payload.name)
             return user
