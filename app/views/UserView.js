cc8dcf13cb1788a5048220f456cade736f6e3fb6
LJosueCM <ljosuecm@gmail.com>
2022-04-20 23:24:36 -0500

    Views Requerimiento 2 Exitoso

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -3,6 +3,9 @@ const UserService = require('../../app/services/UserService')
 class UserView{
     static createUser(payload){
         if(payload === null){return {error:"El payload no existe"}}
+        else if(typeof payload.username != "string" && typeof payload.name != "string" && typeof payload.id != "number"){
+            return {error: "Los valores del payload necesitan tener un valor válido"}
+        }
     }
 }
 
